dbe312c57708dadf22a58f58a261709cf5eaead9
IvanWiesner <ivanwiesner@gmail.com>
2021-11-22 14:41:14 -0700

    native new commands

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+// the speical components features
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+    {/* view = div component */}
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('The Text has Changed!')}/>
     </View>
   );
 }
